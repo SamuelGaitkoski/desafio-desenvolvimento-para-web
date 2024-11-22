394476dfc1c7e63fa7cdcd8b5b834273eaebf5f3
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2024-11-21 21:26:38 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('a[data-page]').on('click', function (e) {
+        e.preventDefault();
+
+        const page = $(this).data('page');
+        $('#content').load(`pages/${page}.html`, function (response, status, xhr) {
+            if (status === "error") {
+                $('#content').html(`<p>Erro ao carregar a página: ${xhr.statusText}</p>`);
+            }
+        });
+
+        $('.nav-item').on('click', function(e) {
+            $('.nav-item').removeClass('active');
+
+            $(this).addClass('active');
+        });
+
+
+    });
+});
